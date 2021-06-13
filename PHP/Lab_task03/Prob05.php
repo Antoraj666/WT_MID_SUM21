00388cf9d410ffd424ddf2b1d0d2781125779aa1
Antoraj666 <antoraj5525@gmail.com>
2021-06-13 12:42:45 +0600

    lab task 3 submitted

--- PHP/Lab_task03/Prob05.php
+++ PHP/Lab_task03/Prob05.php
@@ -1,8 +1,18 @@
 <?php
     $backColor="green"; $fontColor="red"; $imageWidth= "80px"; $imageHeight="80px";
 ?>
-
-<img src="AIUB_whole_logo.png" width= <?php echo  $imageWidth; ?> height=<?php echo  $imageWidth; ?> />
-<h2>American International University -<span style="background-color:<?php echo  $backColor; ?>;color:<?php echo  $fontColor; ?>">
-Bangladesh</h2>
+<html>
+<head></head>
+<body>
+<table>
+<tr>
+<td>
+<img src="AIUB_whole_logo.png" width= <?php echo  $imageWidth; ?> height=<?php echo  $imageWidth; ?> /></td>
+<td>
+<h2>American International <br>University -<span style="background-color:<?php echo  $backColor; ?>;color:<?php echo  $fontColor; ?>">
+Bangladesh</h2></td>
 </span>
+</tr>
+</table>
+</body>
+</html>
